52fbac5b02987ef4b91d3603c5239055b8c3db49
Minecraftschurli <minecraftschurli@gmail.com>
2024-02-14 16:12:33 +0100

    hopefully fix NPE

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 group = "com.github.minecraftschurlimods"
-version = "1.0"
+version = "1.1"
 base.archivesName = "HelperPlugin"
 
 dependencies {
--- src/main/kotlin/com/github/minecraftschurlimods/helperplugin/Util.kt
+++ src/main/kotlin/com/github/minecraftschurlimods/helperplugin/Util.kt
@@ -1,17 +1,15 @@
 package com.github.minecraftschurlimods.helperplugin
 
+import net.neoforged.gradle.util.TransformerUtils
 import org.gradle.api.Project
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.the
 
 // TODO: remove this once https://github.com/gradle/gradle/issues/23572 is fixed
-fun Project.localGradleProperty(name: Provider<String>): Provider<String> = name.map {
-    if (hasProperty(it))
-        property(it)?.toString()!!
-    else
-        null!!
-}
+fun Project.localGradleProperty(name: Provider<String>): Provider<String> = name.map(TransformerUtils.guard {
+    return@guard if (hasProperty(it)) property(it)?.toString() else null
+})
 
 fun Project.localGradleProperty(name: String): Provider<String> = localGradleProperty(provider { name })
 
